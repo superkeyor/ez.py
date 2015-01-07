fdebfd156348304ff63fd1cd4440626ef50d7238
Jerry Zhu <jerryzhujian9@gmail.com>
2015-01-07 11:05:11 -0600

    setup

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/development.html#single-sourcing-the-version
-    version='1.1.1',
+    version='1.1.2',
 
     description='easy stuff',
     long_description=long_description,

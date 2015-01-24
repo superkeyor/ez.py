1023fd2112e29e86e34c92a6127c5e7bfe7af226
Jerry Zhu <jerryzhujian9@gmail.com>
2015-01-24 05:06:49 -0600

    update

--- ez/__init__.py
+++ ez/__init__.py
@@ -1,6 +1,7 @@
 from easyshell import *
 from easyshell import __doc__ as doc1
 from pyg import Mail, mail, AddEvent, addevent, Sheet
+from pyg import Mail2, mail2, AddEvent2, addevent2, Sheet2
 from pyg import __doc__ as doc2
 from version import __version__
 

7e80f91a044cd65c1cc69ca9b54d1547af196e51
Jerry Zhu <jerryzhujian9@gmail.com>
2015-06-28 18:41:16 -0500

    move web-related functions to webly; except pyg (for backward compatibility); webly will also have pyg

--- ez/__init__.py
+++ ez/__init__.py
@@ -4,12 +4,12 @@ from pyg import Mail, mail, AddEvent, addevent, Sheet
 from pyg import Mail2, mail2, AddEvent2, addevent2, Sheet2
 from pyg import __doc__ as doc2
 from version import __version__
-from pyperclip import copy as SetClip
-from pyperclip import copy as setclip
-from pyperclip import paste as GetClip
-from pyperclip import paste as getclip
-from webscraper import Scraper
-from webscraper import __doc__ as doc3
-__doc__ = doc1 + '\n\n\n\n' + doc2 + '\n\n\n\n' + doc3 
-
-del doc1, doc2, doc3
+try:
+    from pyperclip import copy as SetClip
+    from pyperclip import copy as setclip
+    from pyperclip import paste as GetClip
+    from pyperclip import paste as getclip
+except:
+    pass    
+__doc__ = doc1 + '\n\n\n\n' + doc2
+del doc1, doc2

fda5a053d4dbeac7909e4a12fca51a51e30614d8
Jerry Zhu <jerryzhujian9@gmail.com>
2017-11-06 01:45:38 -0600

    update

--- ez/__init__.py
+++ ez/__init__.py
@@ -4,6 +4,7 @@ from pyg import Mail, mail, AddEvent, addevent, Sheet
 from pyg import Mail2, mail2, AddEvent2, addevent2, Sheet2
 from pyg import __doc__ as doc2
 from version import __version__
+from xpinyin import pinyin
 try:
     from pyperclip import copy as SetClip
     from pyperclip import copy as setclip

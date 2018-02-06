458a5cdd5609b7436393279e283e59804470cddd
Jerry Zhu <jerryzhujian9@gmail.com>
2018-02-05 22:43:23 -0600

    update

--- ez/easyshell.py
+++ ez/easyshell.py
@@ -881,11 +881,11 @@ def execute2(cmd, verbose=3, save=None, saveMode='a', redirect=None, redirectMod
                 p = subprocess.Popen(cmd+cmdSuffix, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
             else:
                 # Use a particular shell; the default is sh
-                # tcsh -xef cannot be passed to executable
+                # tcsh -xef 
                 # -x : echo commands to terminal before executing them
                 # -e : terminate script when encountering any error
                 # -f : do not process user's ~/.cshrc file
-                p = subprocess.Popen(cmd+cmdSuffix, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, executable="/bin/"+'tcsh' if shell in ['tcsh'] else shell)
+                p = subprocess.Popen('/usr/bin/env '+'tcsh -xef ' if shell in ['tcsh'] else shell +'-c '+cmd+cmdSuffix, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
             
             # the Popen() instance starts running once instantiated (??)
             # additionally, communicate(), or poll() and wait process to terminate
@@ -931,7 +931,7 @@ def execute2(cmd, verbose=3, save=None, saveMode='a', redirect=None, redirectMod
                     tmp.write(cmd.replace('"','\"').replace("'","\'")+'\n\n') 
             else:
                 with open(save, 'a') as tmp:
-                    tmp.write('#!/usr/bin/env '+'tcsh' if shell in ['tcsh'] else shell+'\n\n'+cmd.replace('"','\"').replace("'","\'")+'\n\n')
+                    tmp.write('#!/usr/bin/env '+'tcsh -xef' if shell in ['tcsh'] else shell+'\n\n'+cmd.replace('"','\"').replace("'","\'")+'\n\n')
             subprocess.call('chmod +x '+save, shell=True)
             print('Command saved at '+save)
 
@@ -953,7 +953,7 @@ def execute2(cmd, verbose=3, save=None, saveMode='a', redirect=None, redirectMod
                     tmp.write(cmd.replace('"','\"').replace("'","\'")+'\n\n') 
             else:
                 with open(save, 'a') as tmp:
-                    tmp.write('#!/usr/bin/env '+'tcsh' if shell in ['tcsh'] else shell+'\n\n'+cmd.replace('"','\"').replace("'","\'")+'\n\n')
+                    tmp.write('#!/usr/bin/env '+'tcsh -xef' if shell in ['tcsh'] else shell+'\n\n'+cmd.replace('"','\"').replace("'","\'")+'\n\n')
             subprocess.call('chmod +x '+save, shell=True)
             print('Command saved at '+save)
         return None
@@ -1237,13 +1237,13 @@ def execute(cmd, verbose=3, save=None, saveMode='a', redirect=None, redirectMode
                 subprocess.call(cmd+cmdSuffix, shell=True, stdout=open(os.devnull, "w"), stderr=subprocess.STDOUT)
         else:
             if output:
-                # tcsh -xef cannot be passed to executable
+                # tcsh -xef 
                 # -x : echo commands to terminal before executing them
                 # -e : terminate script when encountering any error
                 # -f : do not process user's ~/.cshrc file
-                subprocess.call(cmd+cmdSuffix, shell=True, executable="/bin/"+'tcsh' if shell in ['tcsh'] else shell)    # Use bash; the default is sh
+                subprocess.call('/usr/bin/env '+'tcsh -xef ' if shell in ['tcsh'] else shell +'-c '+cmd+cmdSuffix, shell=True)    # Use bash; the default is sh
             else:
-                subprocess.call(cmd+cmdSuffix, shell=True, executable="/bin/"+'tcsh' if shell in ['tcsh'] else shell, stdout=open(os.devnull, "w"), stderr=subprocess.STDOUT)
+                subprocess.call('/usr/bin/env '+'tcsh -xef ' if shell in ['tcsh'] else shell +'-c '+cmd+cmdSuffix, shell=True, stdout=open(os.devnull, "w"), stderr=subprocess.STDOUT)
         print ""
 
         # save even if not run successfully
@@ -1254,7 +1254,7 @@ def execute(cmd, verbose=3, save=None, saveMode='a', redirect=None, redirectMode
                     tmp.write(cmd.replace('"','\"').replace("'","\'")+'\n\n') 
             else:
                 with open(save, 'a') as tmp:
-                    tmp.write('#!/usr/bin/env '+shell+'\n\n'+cmd.replace('"','\"').replace("'","\'")+'\n\n')
+                    tmp.write('#!/usr/bin/env '+'tcsh -xef' if shell in ['tcsh'] else shell+'\n\n'+cmd.replace('"','\"').replace("'","\'")+'\n\n')
             subprocess.call('chmod +x '+save, shell=True)
             print('Command saved at '+save)
     else:
@@ -1266,7 +1266,7 @@ def execute(cmd, verbose=3, save=None, saveMode='a', redirect=None, redirectMode
                     tmp.write(cmd.replace('"','\"').replace("'","\'")+'\n\n') 
             else:
                 with open(save, 'a') as tmp:
-                    tmp.write('#!/usr/bin/env '+shell+'\n\n'+cmd.replace('"','\"').replace("'","\'")+'\n\n')
+                    tmp.write('#!/usr/bin/env '+'tcsh -xef' if shell in ['tcsh'] else shell+'\n\n'+cmd.replace('"','\"').replace("'","\'")+'\n\n')
             subprocess.call('chmod +x '+save, shell=True)
             print('Command saved at '+save)
 

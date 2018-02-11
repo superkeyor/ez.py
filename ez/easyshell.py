85157726b9062acd3e85ab977e4b212e34a3957a
Jerry Zhu <jerryzhujian9@gmail.com>
2018-02-11 12:14:48 -0600

    update

--- ez/easyshell.py
+++ ez/easyshell.py
@@ -1496,7 +1496,7 @@ queue
     print('Condor submit file saved at '+submitfile)
 
     if email:
-        cmd = "(condor_submit %s; condor_wait %s; printf '%s' | mail -s 'Condor run complete. Scheduled on %s' %s) &" % (submitfile,log,'parentdir: %s\n'+Moment().time+'\n'+'\n'.join(executables)+' $(du -csh ../ | grep total | awk "{print $1}")',Moment().date,email)
+        cmd = "(condor_submit %s; condor_wait %s; printf '%s' %s | mail -s 'Condor run complete. Scheduled on %s' %s) &" % (submitfile,log,'parentdir: %s\n'+Moment().time+'\n'+'\n'.join(executables),' $(du -csh ../ | grep total | awk "{print $1}")',Moment().date,email)
     else:
         cmd = "condor_submit %s" % submitfile
     

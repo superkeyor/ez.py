6383a062a21f23095e742f43ec921d2c0b534ea0
Jerry Zhu <jerryzhujian9@gmail.com>
2019-12-20 16:33:11 -0500

    update

--- ez/prepublish.py
+++ ez/prepublish.py
@@ -7,11 +7,6 @@ MODULE_PATH = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(1, MODULE_PATH)
 here = path.abspath(path.dirname(__file__))
 
-# readme
-from __init__ import __doc__ as doc
-with open(path.join(os.path.dirname(here), 'README.rst'), encoding='utf-8', mode='w') as f:
-    f.write(doc)
-
 # version
 from version import __version__ as ver
 ver = ver.replace('.','')

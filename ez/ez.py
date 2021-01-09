b9b624201eb2aa2d75d1124faf8ef47347af8a73
Jerry Zhu <jerryzhujian9@gmail.com>
2021-01-08 19:53:35 -0600

    update

--- ez/ez.py
+++ ez/ez.py
@@ -3175,7 +3175,7 @@ writep = savep
 def readj(file='data.json',*args,**kwargs):
     import json
     with open(file, 'r') as f:
-        return json.load(f,*args,**kwargs)
+        return json.load(f.read(),*args,**kwargs)
 
 def savej(x,file='data.json',indent=4,*args,**kwargs):
     """

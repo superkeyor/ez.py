e9009c8f624e00fc5b241e821282abbbd6d2dc45
Jerry Zhu <jerryzhujian9@gmail.com>
2021-04-21 20:44:01 -0500

    publish

--- publish.sh
+++ publish.sh
@@ -27,18 +27,15 @@ python setup.py sdist upload
 
 rm -r *.egg-info
 
-# /Users/jerry/Library/Enthought/Canopy_64bit/User/bin/pip install $package --upgrade
-# /Users/jerry/PyEnv/bin/pip install $package --upgrade
-# /Library/Frameworks/Python.framework/Versions/2.7/bin/pip install $package --upgrade
-source ~/.bash_profile
-pip install $package --upgrade --no-cache-dir --disable-pip-version-check
-
-# do not use this, seems not working??
-# Although not required, it’s common to locally install your project in “develop” or “editable” mode 
-# while you’re working on it. This allows the project to be both installed and editable in project form.
-# run the following:
-# python setup.py develop
-
 git add -A 
 git commit -m 'update' 
 git push origin master 
+
+# /Library/Frameworks/Python.framework/Versions/2.7/bin/pip install $package --upgrade
+source ~/.bash_profile
+pip install $package --upgrade --no-cache-dir --disable-pip-version-check
+# do not use this, seems not working??
+# Although not required, it’s common to locally install your project in "develop" or "editable" mode 
+# while you’re working on it. This allows the project to be both installed and editable in project form.
+# run the following:
+# python setup.py develop

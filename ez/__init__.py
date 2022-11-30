c168760809d5eadb86eed4f00a22cc482523886a
Jerry Zhu <jerryzhujian9@gmail.com>
2022-11-30 15:56:52 -0600

    update

--- ez/__init__.py
+++ ez/__init__.py
@@ -4,6 +4,6 @@ from . firefox import *
 
 __all__=[
 'Firefox', 'BY', 'KEYS',
-'JDict', 'TimeStamp', 'Moment', 'GSheet', 
+'JDict', 'TimeStamp', 'Moment', 'GSheet', 'GCal'
 'fire', 'requests', 'sqlite3', 'json', 'io', 'os', 'sys', 'platform', 'string', 'random', 're', 'datetime', 'tzlocal', 'pd', 'np', 'urlparse', 'BeautifulSoup', 
 ]
